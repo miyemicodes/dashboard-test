35edd4c67d47026fbdde988b49bee3ba8bba5164
miyemicodes <lizzymiemi@gmail.com>
2024-12-23 18:19:15 +0100

    again

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -10,11 +10,7 @@ interface ModalProps {
 	title?: string;
 }
 
-export default function Modal({
-	open,
-	onClose,
-	children,
-}: ModalProps) {
+export default function Modal({ open, onClose, children }: ModalProps) {
 	const [mounted, setMounted] = useState(false);
 	const dialogRef = useRef<HTMLDialogElement | null>(null);
 
@@ -48,26 +44,28 @@ export default function Modal({
 			className="modal fixed inset-0 z-50 flex items-center justify-center bg-black/50 w-full h-full"
 			onClick={(e) => e.target === dialogRef.current && onClose()}
 		>
-			<div className="bg-[#efefef] rounded-lg shadow-lg max-w-lg md:max-w-3xl  w-full">
-				<header className="p-4 flex justify-between items-center">
-					<button
-						onClick={onClose}
-						aria-label="Close Modal"
-						className="text-gray-500 hover:text-gray-700 text-xl"
-					>
-						&times;
-					</button>
-				</header>
+			<div className="p-[0.125rem] mt-14 md:mt-2 bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 rounded-xl ">
+				<div className="bg-[#efefef] rounded-lg shadow-lg max-w-lg md:max-w-3xl  w-full">
+					<header className="p-4 flex justify-between items-center">
+						<button
+							onClick={onClose}
+							aria-label="Close Modal"
+							className="text-gray-500 hover:text-gray-700 text-xl"
+						>
+							&times;
+						</button>
+					</header>
 
-				<main className="p-4">{children}</main>
-				<footer className="p-4 flex justify-end">
-					<button
-						onClick={onClose}
-						className="px-4 py-2 bg-slate-50 text-slate-800 border-2 hover:text-white rounded hover:bg-blue-600"
-					>
-						Close
-					</button>
-				</footer>
+					<main className="p-4">{children}</main>
+					<footer className="p-4 flex justify-end">
+						<button
+							onClick={onClose}
+							className="px-4 py-2 bg-slate-50 text-slate-800 border-2 hover:text-white rounded hover:bg-blue-600"
+						>
+							Close
+						</button>
+					</footer>
+				</div>
 			</div>
 		</dialog>,
 		document.getElementById("modal-root")!
